def9c922e494b9fd614f3a367ba71175376b479b
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-26 10:51:10 +0100

    Update products.js

--- components/products.js
+++ components/products.js
@@ -1,28 +1,28 @@
 const products_db = {
   'hdj5fr8d': {
     title: 'Adidas XZ', 
-    image: '../media/images/zx_2k_boost.png',
+    image: 'media/images/zx_2k_boost.png',
     image_bg: 'linear-gradient( to right, rgba(245, 70, 66, 0.75), rgba(8, 83, 156, 0.75) )',
     sizes: [39, 40, 42, 43],
     description: 'The best shoe to discover the enegrie that you have.'
   },
   'fr6hibm': {
     title: 'Adidas Lxcon', 
-    image: '../media/images/lxcon.png',
+    image: 'media/images/lxcon.png',
     image_bg: '#ff5734b8',
     sizes: [39, 40, 42, 43],
     description: 'Shoe than compine between sport and luxury.'
   },
   'ju86trh': {
     title: 'Adidas Nite jogger', 
-    image: '../media/images/nite_jogger.png',
+    image: 'media/images/nite_jogger.png',
     image_bg: '#51ea6bb3',
     sizes: [39, 40, 42, 43],
     description: 'Awesome shoe.'
   },
   'm9wZ7WGk': {
     title: 'Adidas Superstar', 
-    image: '../media/images/superstar.png',
+    image: 'media/images/superstar.png',
     image_bg: 'linear-gradient( to right, #f4aad7bf, #01dbf0ab )',
     sizes: [39, 40, 42, 43],
     description: 'To be a unique superstar.'
