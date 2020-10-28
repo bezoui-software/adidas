9ce54761c8dc6ac34d23c350e880ee252a73eddd
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-28 15:32:28 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,12 +1,20 @@
 function Header() {
+  const toggleMobileNavbar = () => document.querySelector('header #mobile-navbar').classList.toggle('hide-mobile-navbar');
+
   return (
     <header>
       <div id='logo'>Adidas</div>
-      <nav>
-        <Link to='/adidas' className='link'>Products</Link>
-        <Link to='/adidas' className='link'>Contact</Link>
-        <Link to='/adidas' className='link'>About</Link>
+      <nav id='laptop-navbar'>
+        <Link to='/' className='link'>Products</Link>
+        <Link to='/' className='link'>Contact</Link>
+        <Link to='/' className='link'>About</Link>
       </nav>
+      <nav id='mobile-navbar' className='hide-mobile-navbar'>
+        <Link to='/' className='link'>Products</Link>
+        <Link to='/' className='link'>Contact</Link>
+        <Link to='/' className='link'>About</Link>
+      </nav>
+      <div id='mobile-navbar-toggle' class='material-icons' onClick={toggleMobileNavbar}>menu</div>
     </header>
   );
 }
