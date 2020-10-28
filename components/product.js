3753b4ee3122335ced2c40d8073845d8d6e0b7b2
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-28 14:11:16 +0100

    Update product.js

--- components/product.js
+++ components/product.js
@@ -1,12 +1,10 @@
 function Product({id, title, image, image_bg}) {
   return (
-    <Link to={`product/${id}`}>
-      <div class='product-container'>
-        <div id='image-container' style={{background: `${image_bg}`}}>
-          <img id='image' src={image}/>
-        </div>
-        <div id='title'> {title} </div>
+    <Link to={`product/${id}`} class='product-container'>
+      <div id='image-container' style={{background: `${image_bg}`}}>
+        <img id='image' src={image}/>
       </div>
+      <div id='title'> {title} </div>
     </Link>
   );
 }
